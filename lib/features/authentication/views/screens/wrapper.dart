11f82fcf77d59b17f948fd852bcbdd9041521085
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-24 14:33:08 +0100

    added page

---
+++ lib/features/authentication/views/screens/wrapper.dart
@@ -0,0 +1,14 @@
+import 'package:book_keeping_mobile/cores/components/custom_scaffold_widget.dart';
+import 'package:flutter/material.dart';
+
+class WrapperScreen extends StatelessWidget {
+  const WrapperScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return scaffold(
+      body: const Center(child: Text('home'))
+      
+    );
+  }
+}
