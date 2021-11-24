0b8ac48dfbf206aa028f8b8cf04ad2d30758fee7
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-24 14:37:44 +0100

    updates

--- lib/pages_list.dart
+++ lib/pages_list.dart
@@ -1,6 +1,9 @@
+import 'package:book_keeping_mobile/features/authentication/views/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 List<GetPage<Widget>> pages() {
-  return <GetPage<Widget>>[];
+  return <GetPage<Widget>>[
+    GetPage(name: '/', page: () => const WrapperScreen()),
+  ];
 }
